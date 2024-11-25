d047f23e3164b223d036c531c7cb4525f468ed36
Lau Tsz Yin <bob85670@connect.hku.hk>
2024-11-25 21:09:55 +0800

    angular standalone components feature let us don't need app.module.ts now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { WishItem } from '../shared/models/wishItem'; 
+import { FormsModule } from '@angular/forms';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  items : WishItem[] = [
+  items: WishItem[] = [
     new WishItem('To Learn Angular'),
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself')
   ];
+
+  newWishText = '';
+
   title = 'wishlist';
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
   
-  toggleItem(item : WishItem) {
+  toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
   }
