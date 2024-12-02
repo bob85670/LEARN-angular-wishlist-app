10387f7770b1773c13d90dd60f42e4849d7c9f1f
Your Name <bob85670@connect.hku.hk>
2024-12-02 22:11:40 +0800

    use ngModelChange,  means event data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { WishItem } from '../shared/models/wishItem';
 import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
-
-const filters = [
-  (item : WishItem) => item,
-  (item : WishItem) => !item.isComplete,
-  (item : WishItem) => item.isComplete
-]
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, WishListComponent, AddWishFormComponent],
+  imports: [RouterOutlet, CommonModule, FormsModule, 
+    WishListComponent, AddWishFormComponent, WishFilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -26,13 +22,6 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ];
 
-  listFilter : any = '0';
-
-  title = 'wishlist';
-
-  get visibleItems() : WishItem[] {
-    return this.items.filter(filters[this.listFilter]);
-  };
-
+  filter: any = () => [];
   
 }
