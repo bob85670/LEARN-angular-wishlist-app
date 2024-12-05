219f1bd79ed3c44aaaa5a19bc36fc53318169afa
Your Name <bob85670@connect.hku.hk>
2024-12-05 22:12:24 +0800

    more on http request

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,30 @@ export class WishService {
 
   constructor(private http: HttpClient) { }
 
+  private getStandardOptions() : any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }
+  }
+
   getWishes() {
-    return this.http.get('assets/wishes.json');
+    let options = this.getStandardOptions();
+
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json'
+      } 
+    })
+
+    return this.http.get('assets/wishes.json', options);
+  }
+
+  private addWish(wish: any) { 
+    let options = this.getStandardOptions();
+
+    options.headers = options.headers.set('Authorization', 'value-need-for-authorization');
+    this.http.post('assets/wishes.json', wish, options);
   }
 }
